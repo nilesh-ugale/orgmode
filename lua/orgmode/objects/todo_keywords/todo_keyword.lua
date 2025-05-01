42528149144d448f0e3d52da309fde8b1105f241
Nilesh Ugale <nilesh.ugale@sedemac.com>
2025-05-01 12:08:31 +0530

    wip:

--- lua/orgmode/objects/todo_keywords/todo_keyword.lua
+++ lua/orgmode/objects/todo_keywords/todo_keyword.lua
@@ -8,6 +8,8 @@
 ---@field shortcut string
 ---@field hl string
 ---@field has_fast_access boolean
+---@field log_state_entry string
+---@field log_state_exit string
 local TodoKeyword = {}
 TodoKeyword.__index = TodoKeyword
 
@@ -43,11 +45,34 @@ function TodoKeyword:parse()
   self.value = self.keyword
   self.shortcut = self.keyword:sub(1, 1):lower()
 
-  local value, shortcut = self.keyword:match('(.*)%((.)[^%)]*%)$')
+  local value, shortcut, inside = self.keyword:match('(.*)%((.)([^%)]*)%)$')
+  if inside then
+    local special1, special2 = inside:match("([!@])%/([!@])")
+    if special1 then
+      self.log_state_entry = special1
+    else
+      self.log_state_entry = nil
+    end
+    if special2 then
+      self.log_state_exit = special2
+    else
+      self.log_state_exit = nil
+    end
+    if special1 == nil and special2 == nil then
+      local special = inside:match("[!@]")  -- check if ! or @ inside
+      if special then
+        self.log_state_entry = special
+      end
+    end
+  end
   if value and shortcut then
     self.value = value
-    self.shortcut = shortcut
-    self.has_fast_access = true
+    if shortcut ~= "!" and shortcut ~= "@" then
+      self.shortcut = shortcut
+      self.has_fast_access = true
+    else
+      self.log_state_entry = shortcut
+    end
   end
 end
 
